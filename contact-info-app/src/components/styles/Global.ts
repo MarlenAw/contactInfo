e9f6782e87b3bd1174d9d42fa954ba14c138ab9a
Marlen <awwadmarlen@gmail.com>
2022-01-29 13:08:04 +0200

    Create colors interface for styled components

--- contact-info-app/src/components/styles/Global.ts
+++ contact-info-app/src/components/styles/Global.ts
@@ -8,7 +8,6 @@ const GlobalStyled = createGlobalStyle`
     }
 
     body {
-        background: ${({ theme }) => theme.colors.body};
         color: hsl(192, 100%, 9%);
         font-family: 'Poppins', sans-serif;
         font-size: 1.15em;
--- contact-info-app/src/interface/styled.d.ts
+++ contact-info-app/src/interface/styled.d.ts
@@ -7,9 +7,40 @@ declare module 'styled-components' {
     borderRadius: string;
 
     colors: {
-      main: string;
-      secondary: string;
-      body: string;
+      black?: string;
+      blue?: string;
+      blueCloud?: string;
+      blueCloudDark?: string;
+      blueDark?: string;
+      blueDark2?: string;
+      blueLight?: string;
+      blueSky?: string;
+      blueSkyDark?: string;
+      blueSkyLight?: string;
+      green?: string;
+      greenDark?: string;
+      greenForestLight?: string;
+      greenForestDark?: string;
+      greenLight?: string;
+      grey?: string;
+      greyDark?: string;
+      greyDisabled?: string;
+      greyIcons?: string;
+      greyLight?: string;
+      paleBlue2?: string;
+      pink?: string;
+      pinkLight?: string;
+      pinkDark?: string;
+      purple?: string;
+      purpleLight?: string;
+      purpleDark?: string;
+      red?: string;
+      redLight?: string;
+      redDark?: string;
+      white?: string;
+      yellow?: string;
+      yellowLight?: string;
+      yellowDark?: string;
     };
   }
 }
